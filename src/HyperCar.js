35b93bc407e1295c96add84696dc994c4dff9210
Will Harris <Harriswill22@yahoo.com>
2018-12-14 08:02:07 -0500

    creates Hyper Car component

--- src/HyperCar.js
+++ src/HyperCar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const HyperCar= (props) =>{
-    const hyperCarListItem = props.hyperCarList.map(hyperCar =>{
+    const hyperCarListItems = props.hyperCarList.map(hyperCar =>{
         return(
             <li>
                 <Link to={`/hypercar/${hyperCar.name}`}>{hyperCar.name}</Link>
