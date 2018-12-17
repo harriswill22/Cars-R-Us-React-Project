3835253fe4441ea4afc12c69f18f5b3d633f8914
Will Harris <Harriswill22@yahoo.com>
2018-12-17 12:47:49 -0500

    creates div for output

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -3,6 +3,8 @@ import React from 'react';
 const SearchBar = (props) => {
     return (
         <input 
+
+        placeholder="Audi"
             value={props.term}
             onChange={(event) => {
                 props.handleInput(event.target.value);
