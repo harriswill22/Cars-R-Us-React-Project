c2724602adf872fb57dba1ae0260f9a86bd89852
Will Harris <Harriswill22@yahoo.com>
2018-12-17 09:59:10 -0500

    adds car details and pictures

---
+++ src/HyperCarInfo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+const HyperCarInfo =(props) =>{
+    const allInfo = props.hyperCarInfo
+    const carName = props.match.params.hyperCarName
+    const carDetail = allInfo.find((car) =>{
+        return car.name === carName
+    })
+
+    return(
+        <div className="SingleCarPage">
+        <ul>
+        <h1>{carDetail.name}</h1>
+        <img src={carDetail.img} alt={carDetail.name}></img>
+        <h3>Price: {carDetail.price}</h3>
+        <h3>Torque: {carDetail.Torque}</h3>
+        <h3>Horsepower:{carDetail.HorsePower}</h3>
+        </ul>
+        </div>
+    )
+}
+export default HyperCarInfo; 
